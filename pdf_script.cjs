0d7e2b8e214c7c95932845bcfd51cd9e3e74ecad
Tung Nguyen <redstrike91dn@gmail.com>
2026-02-28 02:11:49 +0700

    chore: Remove unused PDF parsing scripts and Svelte check output log.

--- pdf_script.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-
-const dataBuffer = fs.readFileSync('docs/Mega App Shell Plan.pdf');
-pdf(dataBuffer).then(function(data) {
-    console.log(data.text);
-}).catch(function(error) {
-    console.error(error);
-});
--- pdf_script.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import pdf from 'pdf-parse';
-
-const dataBuffer = fs.readFileSync('docs/Mega App Shell Plan.pdf');
-pdf(dataBuffer).then(function(data) {
-    console.log(data.text);
-}).catch(function(error) {
-    console.error(error);
-});
